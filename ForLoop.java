c2bec18c83a2d434cc6f8fffc2ac1728d8cdcf88
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:37:12 -0500

    Basic for loops

---
+++ ForLoop.java
@@ -0,0 +1,26 @@
+package week1;
+import java.util.Scanner;
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//  (FOR ALL NUMBERS BETWEEN 5-50)
+		//for(int i = 5; i <= 50; i++) {
+		//	System.out.print(i +" ");
+		//}
+		
+		//(FOR ALL EVEN NUMBERS BETWEEN 10-20)
+		//for(int i = 10; i <= 20; i+=2) {
+		//	System.out.print(i +" ");
+		//}
+		
+		//  (FOR ALL ODD NUMBERS BETWEEN 10-20)
+		for(int i = 11; i <= 20; i+=2) {
+			System.out.print(i +" ");
+		}
+		
+	}
+	
+
+}
