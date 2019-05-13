65245d2ff26d8c59b4f09b74ea7e949076a08832
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:36:00 -0500

    Display the even numbers between two integers

---
+++ EvenNumbersBetween10And20.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class EvenNumbersBetween10And20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		for(int i = 10; i <= 20; i+=2) {
+			System.out.print(i +" ");
+		
+	}
+
+}
+}
