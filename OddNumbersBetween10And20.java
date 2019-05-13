5d42f19d5a934218256f1abf1ee259e1d58ef99c
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:41:19 -0500

    Display odd numbers between two integers

---
+++ OddNumbersBetween10And20.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class OddNumbersBetween10And20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 11; i <= 20; i+=2) {
+			System.out.print(i +" ");
+	}
+
+}
+}
