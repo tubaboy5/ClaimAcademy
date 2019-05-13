a408912cc3f6288f93af76396a5fa195efed0a1c
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:36:44 -0500

    Prints array of stars based on size

---
+++ FiveByFiveStars.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class FiveByFiveStars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i <=4; i++)
+		{
+			for(int j = 0; j<=4; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
