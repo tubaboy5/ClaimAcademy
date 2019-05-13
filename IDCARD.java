407a8e8ddc1feccb6f180c5866971b3c2ac138ea
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:13:37 -0500

    Create IDCARD.java
    Print out a basic ID Card

---
+++ IDCARD.java
@@ -0,0 +1,43 @@
+package Demos;
+
+public class IDCARD {
+	
+	public static void main(String[] args) {
+		System.out.println("Starting Welcome message"); 
+		
+		
+		// Declare and define the variable
+		String name = "Roger Williams";
+		String ID = "ST05GH23";
+		String state = "CA";
+		String expirationDate = "12/2023";
+		
+		
+		// Use the variable
+		System.out.println(name);
+		System.out.println(ID);
+		System.out.println(state);
+		System.out.println(expirationDate);
+		
+		// Print message
+		System.out.println("ID Card for " + name +" of " + state + "");
+		System.out.println(" ");
+		System.out.println("NAME: " + name + "");
+		System.out.println("ID: " + ID + "");
+		System.out.println("STATE: " + state + "");
+		System.out.println("EXP. DATE: " + expirationDate + "");
+	
+		
+		
+		// ID Card Printer
+		// Create an application that prints an ID card. 
+		// Use appropriate variables for: Name, ID, State, Expiration Date. 
+		
+		// Example: 
+		// NAME: Roger Williams 
+		// ID: ST05GH23 
+		// STATE: CA 
+		// EXP. DATE: 12/2023
+	}
+
+}
