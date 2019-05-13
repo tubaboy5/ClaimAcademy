0ad49f1892907c6040abcbdf2a9267c300e0337b
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:29:40 -0500

    Print numbers between two integers

---
+++ AllNumbersBetween5And15.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class AllNumbersBetween5And15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i = 5; i <= 15; i++) {
+				System.out.print(i +" ");
+	}
+
+	}
+
+}
