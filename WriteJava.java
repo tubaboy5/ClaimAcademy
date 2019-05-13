0b5ce6a5b0138dee16e4a8edafbf09bedb4c08a0
Justin Hartmann <tubaboy5@gmail.com>
2019-05-13 06:26:07 -0500

    Create a program that displays JAVA with text characters

---
+++ WriteJava.java
@@ -0,0 +1,20 @@
+/*
+ * (Displaying a pattern) Write a program that displays the following pattern:
+     J      A     V     V     A
+     J     A A     V   V     A A
+J    J    AAAAA     V V     AAAAA
+ J J     A     A     V     A     A
+ */
+package chapter1;
+
+public class WriteJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("     J      A     V     V     A");
+		System.out.println("     J     A A     V   V     A A");
+		System.out.println("J    J    AAAAA     V V     AAAAA");
+		System.out.println(" J J     A     A     V     A     A");
+	}
+
+}
